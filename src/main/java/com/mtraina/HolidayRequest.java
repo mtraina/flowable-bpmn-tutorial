2f55597710f1b017dfd8936583774d3a63cf3c13
mtraina <matteo.traina.mail@gmail.com>
2025-01-14 22:27:25 +0100

    first run

--- src/main/java/com/mtraina/HolidayRequest.java
+++ src/main/java/com/mtraina/HolidayRequest.java
@@ -2,19 +2,57 @@ package com.mtraina;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+
+import java.util.Scanner;
 
 public class HolidayRequest {
 
     public static void main(String[] args) {
-        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
-          .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
-          .setJdbcUsername("sa")
-          .setJdbcPassword("")
-          .setJdbcDriver("org.h2.Driver")
-          .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-    
+        init();
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Who are you?");
+        String employee = scanner.nextLine();
+
+        System.out.println("How many holidays do you want to request?");
+        Integer nrOfHolidays = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Why do you need them?");
+        String description = scanner.nextLine();
+    }
+
+    private static void init(){
+        final var configuration = createConfiguration();
+        createProcessDefinition(configuration);
+    }
+
+    private static ProcessEngineConfiguration createConfiguration(){
+        return new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+    }
+
+    private static ProcessDefinition createProcessDefinition(ProcessEngineConfiguration cfg){
         ProcessEngine processEngine = cfg.buildProcessEngine();
-      }
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .deploy();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        System.out.println("Found process definition : " + processDefinition.getName());
+        return processDefinition;
+    }
 
 }
