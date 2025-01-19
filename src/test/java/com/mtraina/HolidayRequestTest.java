37b4137fd7d1c9410477834592af04f0df8d75c6
mtraina <matteo.traina.mail@gmail.com>
2025-01-19 11:16:22 +0100

    unit test added I

---
+++ src/test/java/com/mtraina/HolidayRequestTest.java
@@ -0,0 +1,41 @@
+package com.mtraina;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.test.ConfigurationResource;
+import org.flowable.engine.test.Deployment;
+import org.flowable.engine.test.FlowableTest;
+import org.flowable.task.api.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@FlowableTest
+@ConfigurationResource("holiday-request.bpmn20.xml")
+class HolidayRequestTest {
+
+    private ProcessEngine processEngine;
+    private RuntimeService runtimeService;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp(ProcessEngine processEngine) {
+        this.processEngine = processEngine;
+        this.runtimeService = processEngine.getRuntimeService();
+        this.taskService = processEngine.getTaskService();
+    }
+
+    @Test
+    @Deployment
+    void testSimpleProcess() {
+        runtimeService.startProcessInstanceByKey("simpleProcess");
+
+        Task task = taskService.createTaskQuery().singleResult();
+        assertEquals("My Task", task.getName());
+
+        taskService.complete(task.getId());
+        assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+    }
+}
